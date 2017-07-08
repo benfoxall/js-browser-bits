c662750ff15bce4dce6b420144fac426e9091f5f
Ben Foxall <benfoxall@gmail.com>
2017-07-08 21:59:32 +0100

    I might not actually use this slide actually

--- docs/scripts/CountDecimal.js
+++ docs/scripts/CountDecimal.js
@@ -5,25 +5,21 @@ window.CountDecimal = (function(Reveal, SlideBuilder) {
 
     const root = document.querySelector(selector)
 
-    const numberElement = root.querySelector('.count-value')
-    numberElement.textContent = '0'
+    const decElement = root.querySelector('.output-dec')
+    decElement.textContent = '0'
+
+    const binElement = root.querySelector('.output-bin')
+    binElement.textContent = '0'
 
     const builder = new SlideBuilder(root)
 
+    const value = {
+      dec: 0,
+      bin: 0
+    }
+
     let rAF
     builder.shown(() => {
-      const data = {number: 0}
-      var tween = new TWEEN.Tween(data)
-      	.to({ number: 1023.99}, 10000)
-      	.onUpdate(function() {
-          numberElement.innerText = data.number|0
-          // numberElement.innerText = (data.number|0).toString(2)
-      	})
-        .repeat(Infinity)
-        .yoyo(true)
-        .easing(TWEEN.Easing.Quartic.InOut)
-        .delay(1000)
-      	.start()
 
       function animate(t) {
         rAF = requestAnimationFrame(animate)
@@ -33,6 +29,44 @@ window.CountDecimal = (function(Reveal, SlideBuilder) {
       rAF = requestAnimationFrame(animate)
 
     })
+    .fragments([
+      () => {
+        new TWEEN.Tween(value)
+          .to({ dec: 9}, 1000)
+          .onUpdate(function() {
+            decElement.innerText = value.dec|0
+          })
+          .easing(TWEEN.Easing.Linear.None)
+          .start()
+      },
+      () => {
+        new TWEEN.Tween(value)
+          .to({ dec: 99999999}, 10000)
+          .onUpdate(function() {
+            decElement.innerText = value.dec|0
+          })
+          .easing((k) => Math.pow(k, 10))
+          .start()
+      },
+      () => {
+        new TWEEN.Tween(value)
+          .to({ bin: 1}, 1000)
+          .onUpdate(function() {
+            binElement.innerText = value.bin|0
+          })
+          .easing((k) => Math.pow(k, 10))
+          .start()
+      },
+      () => {
+        new TWEEN.Tween(value)
+          .to({ bin: 0b1111111111111111}, 10000)
+          .onUpdate(function() {
+            binElement.innerText = (value.bin|0).toString(2)
+          })
+          .easing((k) => Math.pow(k, 2))
+          .start()
+      }
+    ])
     .hidden(() => {
       cancelAnimationFrame(rAF)
     })
