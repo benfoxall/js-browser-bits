d3661685866c4e7257ef11f65095c4d1052db863
Ben Foxall <benfoxall@gmail.com>
2017-07-08 21:25:12 +0100

    Tween js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp')
+var rollup = require('rollup-stream')
+var source = require('vinyl-source-stream')
 
 // this is like my 4th take on this
 gulp.task('postinstall', () =>
   gulp.src([
       'node_modules/reveal.js/**/*',
-      'node_modules/slide-builder/slide-builder.js'
+      'node_modules/slide-builder/slide-builder.js',
+      'node_modules/@tweenjs/tween.js/src/Tween.js',
     ],{ "base" : "node_modules" })
   .pipe(gulp.dest('docs/lib'))
 )
