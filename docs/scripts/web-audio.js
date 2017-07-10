8fce3287f07968cdeb82e2eef2b29e5a0fb5638d
Ben Foxall <benfoxall@gmail.com>
2017-07-11 00:31:33 +0100

    Pull out common code

--- docs/scripts/web-audio.js
+++ docs/scripts/web-audio.js
@@ -2,51 +2,64 @@
   if(!(Reveal && SlideBuilder)) throw "Reveal and Slide Builder required"
 
 
-  const root = document.querySelector('#audio-freq-data')
-  const output_element = root.querySelector('.output')
-  let audio_element = root.querySelector('audio')
+  const createAudioSlide = (root, setup, render) => {
 
-  const builder = new SlideBuilder(root)
+    let audio_element = root.querySelector('audio')
 
-  let rAF, audioCtx
-  builder.shown(() => {
+    const builder = new SlideBuilder(root)
 
-    console.log("creating context")
-    audioCtx = new AudioContext()
+    let rAF, audioCtx
+    builder.shown(() => {
+      console.log("creating audio context")
 
-    const source = audioCtx.createMediaElementSource(audio_element)
-    const analyser = audioCtx.createAnalyser()
+      audioCtx = new AudioContext()
 
-    source.connect(analyser)
-    analyser.connect(audioCtx.destination)
+      const source = audioCtx.createMediaElementSource(audio_element)
+      const analyser = audioCtx.createAnalyser()
 
-    var freqData = new Uint8Array(analyser.frequencyBinCount)
-    analyser.getByteFrequencyData(freqData)
+      source.connect(analyser)
+      analyser.connect(audioCtx.destination)
 
-    var subFreq = freqData.subarray(0, 64)
+      setup(analyser)
 
-    const more = `, …(${freqData.length - subFreq.length} more)`
-
-    function animate(t) {
+      function animate(t) {
+        rAF = requestAnimationFrame(animate)
+        render(analyser)
+      }
       rAF = requestAnimationFrame(animate)
+    })
+    .hidden(() => {
+      cancelAnimationFrame(rAF)
+      audioCtx.close()
+        .then(() => console.log("audio context closed"))
 
+      // replace the audio element with a fresh replacement
+      audio_element.replaceWith(audio_element = audio_element.cloneNode())
+    })
+
+  }
+
+
+  const freqSlide = document.querySelector('#audio-freq-data')
+  const output_element = freqSlide.querySelector('.output')
+
+  let freqData, subFreq, more
+
+  createAudioSlide(
+    freqSlide,
+    (analyser) => {
+      freqData = new Uint8Array(analyser.frequencyBinCount)
+      subFreq = freqData.subarray(0, 64)
+      more = `, …(${freqData.length - subFreq.length} more)`
+    },
+    (analyser) => {
       analyser.getByteFrequencyData(freqData)
 
       output_element.innerText =
         [].map.call(subFreq,(n)=>n.toString().padStart(3,'\xa0')).join(', ')
        + more
-
     }
-    rAF = requestAnimationFrame(animate)
-  })
-  .hidden(() => {
-    cancelAnimationFrame(rAF)
-    audioCtx.close()
-      .then(() => console.log("audio context closed"))
-
-    // replace the audio element with a fresh replacement
-    audio_element.replaceWith(audio_element = audio_element.cloneNode())
-  })
+  )
 
 
 
